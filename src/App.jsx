eff8d99f05e18a8d5b7444f3370f901d71fa5fc4
dhvanibhesaniya-inventyv <dhvanibhesaniyainventyv@gmail.com>
2024-04-04 16:19:36 +0530

    Created header component and several pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import About from './pages/About';
+import Signout from './pages/Signout';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <h1 className='text-emerald-600'>
-      App
-    </h1>
-  )
+    <BrowserRouter>
+
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-out" element={<Signout/>}/>
+        <Route path="/sign-in" element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }

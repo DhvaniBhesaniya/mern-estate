82de51fd01d6d687abbb1cd01d0ef3ecc1083458
dhvanibhesaniya-inventyv <dhvanibhesaniyainventyv@gmail.com>
2024-04-10 10:39:21 +0530

    created new api file and connection with mongo db.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,6 @@
 // import React from 'react'
 import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-import moment from 'moment'
 export default function Header() {
 
   return (

5847fc6f767bce04941b1e2740977fdfbf2fee9f
dhvanibhesaniya-inventyv <dhvanibhesaniyainventyv@gmail.com>
2024-04-12 12:15:33 +0530

    Add Redux for state management
    The diff includes changes to:
    
    - `client/package-lock.json` and `client/package.json` to add `@reduxjs/toolkit`, `react-redux

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { Link,useNavigate } from "react-router-dom";
+import { useDispatch ,useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 export default function Signin() {
 
 const [formData, setFormData] = useState({});
-const [ error, setError ] = useState(null);
-const [loading, setLoading] = useState(false);
+ const { error, loading } = useSelector((state) => state.user);
 const navigate = useNavigate();
+const dispatch = useDispatch();
 
   const handleChange = (e) =>{
     setFormData({
@@ -19,7 +21,7 @@ const navigate = useNavigate();
 const handleSubmit = async (e) =>{
   e.preventDefault();
   try {
-    setLoading(true)
+     dispatch(signInStart());
     const res = await fetch('/api/auth/signin',
   
   {
@@ -31,18 +33,16 @@ const handleSubmit = async (e) =>{
   });
   const data = await res.json();
   if (data.success === false) {
-    setError(data.message);
-    setLoading(false);
+    dispatch(signInFailure(data.message));
     return;
   }
   // console.log(data);
-  setLoading(false);
-  setError(null);
+ dispatch(signInSuccess(data));
   navigate('/')
     
   } catch (error) {
-    setLoading(false);
-    setError(error.message);
+    dispatch(signInFailure(error.message));
+     
   }
 }
 

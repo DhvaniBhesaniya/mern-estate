fbfbb7ca22d73349db339faa1f9f771124fd54c9
dhvanibhesaniya-inventyv <dhvanibhesaniyainventyv@gmail.com>
2024-04-16 11:25:41 +0530

    "Added PrivateRoute for profile page and updated header to display user avatar if logged in"
    This commit includes changes to both App.jsx and Header.jsx. In App.jsx, a PrivateRoute component is added around

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Signin from './pages/Signin';
-import About from './pages/About';
-import Signup from './pages/Signup';
-import Header from './components/Header';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-up" element={<Signup/>}/>
+        <Route path="/sign-up" element={<Signup />} />
         <Route path="/sign-in" element={<Signin />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );

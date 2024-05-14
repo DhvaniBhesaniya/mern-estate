4252253c10f3c9f9955347bef65a3060f1ed8651
dhvanibhesaniya-inventyv <dhvanibhesaniyainventyv@gmail.com>
2024-05-14 16:57:16 +0530

    complete listing page with contact funcnalities

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -66,3 +66,19 @@ res.status(200).json(listings);
     return next(errorHandler(401, " you can view only your listings"));
   }
 };
+
+
+export const getUser = async (req,res,next) =>{
+try {
+  const user = await User.findById(req.params.id);
+
+  if(!user) return next(errorHandler(404,"user not found!"));
+
+  const { password:pass, ...rest } = user._doc;
+
+  res.status(200).json(rest);
+  
+} catch (error) {
+  next(error);
+}
+}
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -9,11 +9,11 @@ import {
   FaBath,
   FaBed,
   FaChair,
-  FaMapMarkedAlt,
   FaMapMarkerAlt,
   FaParking,
   FaShare,
 } from "react-icons/fa";
+import Contact from "../components/Contact.jsx";
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
 
@@ -23,9 +23,14 @@ export default function Listing() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
   const params = useParams();
-  const { currentUser } = useSelector((state) => state.user);
-console.log(currentUser)
+  const currentUser = useSelector((state)=> state.user);
+
+
+
+
+
   useEffect(() => {
     const fetchListing = async () => {
       try {
@@ -132,14 +137,15 @@ console.log(currentUser)
                 {listing.furnished ? "Furnished" : "Unfurnished"}
               </li>
             </ul>
-            {currentUser && listing.userRef !== currentUser._id && !contact && (
-              <button
-                onClick={() => setContact(true)}
-                className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
-              >
-                Contact landlord
-              </button>
-            )}
+            {
+              currentUser && listing.userRef !== currentUser._id && !contact &&(
+
+            <button onClick={()=>setContact(true)} className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3">
+              Contact landlord
+            </button>
+              ) 
+            }
+            {contact && <Contact listing={listing}/>}
           </div>
         </div>
       )}
